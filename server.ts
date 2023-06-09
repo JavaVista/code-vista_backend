4adb2d6aaf21dc911ae6e163043faf93f47b960c
Javier 'Javi' Carrion <javier.carrion@designinteractive.net>
2023-06-08 21:00:41 -0400

    feat: :sparkles: add route endpoint to store/remove cookie

--- server.ts
+++ server.ts
@@ -23,9 +23,55 @@ app.use(cors({
 }));
 app.use(bodyParser.json());
 
+// health check endpoint
 app.get(route + "/health", async (req: Request, res: Response) => {
     return res.status(200).json({
         message: "healthy",
+    });
+});
+
+// logout check endpoint
+app.get(route + "/logout", async (req: Request, res: Response) => {
+    res.cookie("server-access-token", { expires: Date.now() });
+    res.cookie("server-refresh-token", { expires: Date.now() });
+    return res.status(200).json({
+        message: "Cookies are expired",
+    });
+});
+
+// store auth cookies 
+app.post(route + "/store-auth", async (req: Request, res: Response) => {
+    if (!req?.body?.accessToken || !req?.body?.refreshToken) {
+        return res.status(401).json({
+            message: "missing token(s)",
+        });
+    }
+
+    const accessToken = req.body.accessToken;
+    const refreshToken = req.body.refreshToken;
+
+    const dateAccessToken = new Date();
+    const dateRefreshToken = new Date();
+
+    dateAccessToken.setHours(dateAccessToken.getHours() + 1);
+    dateRefreshToken.setDate(dateRefreshToken.getDate() + 1);
+
+    res.cookie("server-access-token", accessToken, {
+        secure: process.env.NODE_ENV !== 'development',
+        httpOnly: true,
+        expires: dateAccessToken,
+        sameSite: "lax",
+    });
+
+    res.cookie("server-refresh-token", refreshToken, {
+        secure: process.env.NODE_ENV !== 'development',
+        httpOnly: true,
+        expires: dateRefreshToken,
+        sameSite: "lax",
+    });
+
+    return res.status(200).json({
+        message: "Token(s) stored",
     });
 });
 
